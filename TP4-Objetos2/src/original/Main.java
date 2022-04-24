0f53549207da1413e91989a88475805899ce3aa3
Marii <Marii@192.168.1.13>
2022-04-24 14:22:48 -0300

    V1.0-Empece

---
+++ TP4-Objetos2/src/original/Main.java
@@ -0,0 +1,20 @@
+package original;
+
+import java.awt.EventQueue;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					new AgregarParticipante();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+		});
+	}
+}
